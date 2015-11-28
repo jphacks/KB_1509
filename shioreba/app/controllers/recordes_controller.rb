7ffb3182ef29449fe71c67a1aa96659d1d4f424a
しぇぱっち <shepabashi@gmail.com>
2015-11-29 00:46:30 +0900

    モデルのエラー処理を実装

--- shioreba/app/controllers/recordes_controller.rb
+++ shioreba/app/controllers/recordes_controller.rb
@@ -1,14 +1,15 @@
 class RecordesController < ApplicationController
-	def save_recorde
-	  recode = Recode.new
-	  recode.user_id = params[:user_id]
-	  recode.date = params[:date]
-	  recode.book_id = Book.get_info(params[:isbn])
-	  recode.save
-	  render text: "保存されました?"
-	end
+
+  def save_recorde
+      recode = Recode.new
+      book = Book.get_info(params[:isbn])
+
+      recode.user_id = params[:user_id]
+      recode.date = params[:date]
+      recode.book_id = book.id if book
+      recode.save
+
+      render text: ''
+  end
 end
 
-isbn = "9784774164106"
-user_id = 1
-date = "2015-11-28"
--- shioreba/app/models/recode.rb
+++ shioreba/app/models/recode.rb
@@ -1,4 +1,10 @@
 class Recode < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
+
+  validates :book_id, presence: true
+  validates :date, presence: true
+  validates :user_id, presence: true
+
+
 end
