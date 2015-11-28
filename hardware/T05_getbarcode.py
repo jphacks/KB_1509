45d89c84611f0cf41e0615e4a8f03d37cde0eb90
taiga-sugishita <b1334650@planet.kanazawa-t.ac.jp>
2015-11-29 01:45:28 +0900

    関数　getbarcode() 実装 １実行で１回取得、数字でデータを返す...はず

---
+++ hardware/T05_getbarcode.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import zbar
+import Image
+import time
+import picamera
+
+def getbarcode():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        time.sleep(2) #�J����������
+        camera.capture('barcode.jpg')
+
+    # create a reader
+    scanner = zbar.ImageScanner()
+
+    # configure the reader
+    scanner.parse_config('enable')
+
+    # obtain image data
+    pil = Image.open(barcode.jpg).convert('L')
+    width, height = pil.size
+    raw = pil.tostring()
+
+    # wrap image data
+    image = zbar.Image(width, height, 'Y800', raw)
+
+    # scan the image for barcodes
+    scanner.scan(image)
+
+    # extract results
+    for symbol in image:
+        # do something useful with results
+        print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+
+    # clean up
+    del(image)
+    return symbol.data
+
+codenum = getbarcode();
+print '"%s"' % codenum
