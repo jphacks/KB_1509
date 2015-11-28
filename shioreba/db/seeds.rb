4a9e26c98fa821995c400444891cae7ec91be3bd
Yuta Ide <yIde4949@gmail.com>
2015-11-28 14:37:19 +0900

    [WIP] isbnからデータのデータの取得はできるようになりました. ただ保存する処理がまだできていません.

--- shioreba/db/seeds.rb
+++ shioreba/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.create(:isbn => "9784780802047", :book_name => "おにぎりレシピ101", :book_author => "山田　玲子", :book_lang => "jpn", :book_summry => "", :book_image_url => "http://hanmoto.com/bd/img/9784780802047.jpg")
