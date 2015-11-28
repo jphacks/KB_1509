c7de506acd2b167634e3b6867d1b83a66f9466c0
Atsuki Mizutani <atsu.m0504@gmail.com>
2015-11-29 08:51:48 +0900

    日付取得を追加。

--- hardware/stdinput/stdinput.py
+++ hardware/stdinput/stdinput.py
@@ -4,6 +4,7 @@ __author__ = 'atsu'
 import json
 import urllib
 import urllib2
+import datetime
 
 print('start')
 
@@ -16,18 +17,23 @@ id = 0
 
 while True:
     id += 1
-    inputISBN = raw_input() #
+    inputISBN = raw_input() # 標準入力待ち
+
+    # date値生成
+    now = datetime.datetime.now()
+    date = now.strftime("%Y-%m-%d_%H:%M:%S")
+
 
     # POST送信
     postData = {
-        'date' : '2015-11-28_20:04:00',
+        'date' : date,
         'user_id' : 1192,
         'book_isbn' : inputISBN
     }
     params = urllib.urlencode(postData)   # パーセントエンコード
     print('encoded')
     res = urllib.urlopen(serverURL, params).read()   # POST送信
-    print(res)
+    print(res)  #
     print('end')
 
 #    dic={"id":id, "date":"2015-11-24", "time":"2:05", "ISBN":inputISBN}
