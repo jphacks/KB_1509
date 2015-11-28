52d288625d495948d17e602e522588df7bb59c14
しぇぱっち <shepabashi@gmail.com>
2015-11-29 04:47:09 +0900

    ユーザーの画面に読んだ本が見られる機能を追加

--- shioreba/app/controllers/users_controller.rb
+++ shioreba/app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @user_recordes = Recode.where(user_id: params[:id]).order(date: :desc)
+    @user_recordes = Recode.where(user_id: params[:id]).order(date: :desc, id: :desc)
   end
 
 end
