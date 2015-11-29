b0c94fc021aa6926f9466c0d596fdebe5286d56c
Atsuki Mizutani <atsu.m0504@gmail.com>
2015-11-29 11:39:18 +0900

    その２

--- hardware/regiScanner/stdinput.py
+++ hardware/regiScanner/stdinput.py
@@ -28,27 +28,35 @@ def scan(): #jsonで保存
 
     # JSONファイル書き込み
     # JSON読み込み
-    f = open('record.json', 'r')
-    record = f.read()
-    f.close()
+#    with open('record.json', 'r') as f:
+#        record = json.load(f)
 
+#    f = open('record.json', 'r')
+#    record = json.load(f)
+#    print type(record)
+
+    record = []
     dic={"date":date, "book_isbn":inputISBN}    # 追加辞書オブジェクト
     record.append(dic)   # 配列に追加
 
     # JSON書き込み
-    f = open('record.json', 'w')
-    f.write(record)
-    f.close()
+    with open('record.json', 'w') as f:
+        json.dump(record, f, sort_keys=True, indent=2)
+
+#    f = open('record.json', 'w')
+#    f.write(record)
+#    f.close()
 
 
  #   with open('record.json', 'a') as f:
  #       json.dump(dic, f, sort_keys=True, indent=4)
 
 def send(): # POST送信
-    # JSON読み込み
-    f = open('record.json', 'r')
-    record = f.read()
-    f.close()
+
+
+#    f = open('record.json', 'r')
+#    record = f.read()
+#    f.close()
 
     postData = {
         'date' : date,
